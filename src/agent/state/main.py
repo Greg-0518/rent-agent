428480d059468729874231109a7a517d9887399d
Greg_0518 <1749318218@qq.com>
2026-03-28 21:28:17 +0800

    新增推荐子图状态和主图状态代码

---
+++ src/agent/state/main.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+from langgraph.graph import MessagesState
+
+
+class State(MessagesState):
+    user_intent: str           # 用户意图
+    user_preferences: dict     # 用户偏好
+
+class NeedReserveOutout(TypedDict):
+    reserve: str
