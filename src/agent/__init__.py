45cf15f9b2458d8ca8b975390e2232c9002b18f2
Greg_0518 <1749318218@qq.com>
2026-05-12 23:42:35 +0800

    新增子图流程图像

---
+++ src/agent/__init__.py
@@ -0,0 +1,13 @@
+"""New LangGraph Agent.
+
+This module defines a custom graph
+"""
+
+from src.agent.graph import graph
+from src.agent.recommend import recommended_graph
+from src.agent.reserve import reserve_graph
+from src.agent.extend import extend_graph
+from src.agent.contract import contract_graph
+
+
+__all__ = ["graph", "recommended_graph", "reserve_graph", "extend_graph", "contract_graph"]
